b96f85b482d8db1f6c05802abf03f166c75de388
Mark Jansen <mark.jansen@reactos.org>
2020-09-10 19:30:10 +0200

    [PSDK] Add missing SFC defines

--- sdk/include/psdk/sfc.h
+++ sdk/include/psdk/sfc.h
@@ -25,6 +25,22 @@
 extern "C" {
 #endif
 
+#define SFC_DISABLE_NORMAL 0
+#define SFC_DISABLE_ASK 1
+#define SFC_DISABLE_ONCE 2
+#define SFC_DISABLE_SETUP 3
+#define SFC_DISABLE_NOPOPUPS 4
+
+#define SFC_SCAN_NORMAL 0
+#define SFC_SCAN_ALWAYS 1
+#define SFC_SCAN_ONCE 2
+#define SFC_SCAN_IMMEDIATE 3
+
+#define SFC_QUOTA_DEFAULT 50
+#define SFC_QUOTA_ALL_FILES ((ULONG)-1)
+
+#define SFC_IDLE_TRIGGER L"WFP_IDLE_TRIGGER"
+
 /* Types */
 
 typedef struct _PROTECTED_FILE_DATA {
